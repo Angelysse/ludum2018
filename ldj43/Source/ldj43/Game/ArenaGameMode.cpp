b4cab0ebde7221ed9d69413bf343a2ebc55d3f5e
mrouffet <maximerouffet@gmail.com>
2018-12-01 20:57:03 +0100

    [Add] ProcedGen

--- ldj43/Source/ldj43/Game/ArenaGameMode.cpp
+++ ldj43/Source/ldj43/Game/ArenaGameMode.cpp
@@ -1,7 +1,39 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "ArenaGameMode.h"
+#include "Engine/World.h"
 
+#include "LD/CubeActor.h"
 
+void AArenaGameMode::StartPlay()
+{
+	Super::StartPlay();
 
+	GenerateMap();
+}
 
+void AArenaGameMode::GenerateMap()
+{
+	uint32 gridWidth = _mapWidth / _mapStepWidth;
+	uint32 gridHeight = _mapHeight / _mapStepHeight;
+
+	char* map = new char[gridWidth * gridHeight]{};
+
+	// Create some big blocks.
+	uint32 bigWallNum = FMath::RandRange(_mapMinWalls, _mapMaxWalls);
+
+	for (uint32 i = 0u; i < bigWallNum; i++)
+	{
+		uint32 sizeX = FMath::RandRange(1, 4); uint32 sizeY = FMath::RandRange(1, 4);
+		uint32 posX = FMath::RandRange(0, gridWidth); uint32 posY = FMath::RandRange(0, gridHeight);
+
+		// Fill map.
+		for (uint32 j = 0u; j < sizeY; j++)
+		{
+			for (uint32 k = 0u; k < sizeX; k++)
+				map[posX + posY * gridWidth] = 1;
+		}
+
+		// Spawn Block.
+		auto actor = GetWorld()->SpawnActor<ACubeActor>(FVector(float(posX) - _mapWidth / 2, float(posY) - _mapHeight / 2, 270.0), FRotator::ZeroRotator);
+		actor->SetActorScale3D(FVector(sizeX, sizeY, 5));
+	}
+;}
--- ldj43/Source/ldj43/Game/ArenaGameMode.h
+++ ldj43/Source/ldj43/Game/ArenaGameMode.h
@@ -1,20 +1,30 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Game/GlobalGameMode.h"
 #include "ArenaGameMode.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class LDJ43_API AArenaGameMode : public AGlobalGameMode
 {
 	GENERATED_BODY()
 	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
+	int _mapMinWalls = 25;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
+	int _mapMaxWalls = 125;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
+	int _mapWidth = 9000;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
+	int _mapHeight = 9000;
 	
-	
-	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
+	float _mapStepWidth = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
+	float _mapStepHeight = 1.0f;
+
+	void GenerateMap();
+
+	void StartPlay() override;
 };
