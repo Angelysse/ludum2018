dd50703708a6fb41f5497ce2b4768275cf34cf38
mrouffet <maximerouffet@gmail.com>
2018-12-01 05:37:14 +0100

    [Add] UProject

---
+++ ldj43/Source/ldj43/ldj43GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ldj43GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LDJ43_API Aldj43GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
