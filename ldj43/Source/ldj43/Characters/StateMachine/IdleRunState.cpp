d0b801362cd55d943afaa80f4e986db65218dfeb
mrouffet <maximerouffet@gmail.com>
2018-12-01 14:21:31 +0100

    [Update] L ad R AttackState

--- ldj43/Source/ldj43/Characters/StateMachine/IdleRunState.cpp
+++ ldj43/Source/ldj43/Characters/StateMachine/IdleRunState.cpp
@@ -16,7 +16,7 @@ void IdleRunState::onExitState()
 bool IdleRunState::canSwitchTo(uint8 newState) const
 {
 	return newState == static_cast<uint8>(StateType::JUMP) && Cast<AGlobalPlayerState>(_chara->PlayerState)->canJump && _chara->CanJump()
-		|| newState == static_cast<uint8>(StateType::ATTACK);
+		|| newState == static_cast<uint8>(StateType::RATTACK) || newState == static_cast<uint8>(StateType::LATTACK);
 }
 
 uint8 IdleRunState::getState() const
