8011483d27833e99f84a0f1e33386576c4af4af8
c.fazilleau <c.fazilleau@student.isartdigital.com>
2018-12-02 17:01:18 +0100

    Addicionad la translación

---
+++ ldj43/Source/ldj43/UI/SacrificeSelectorWidget.cpp
@@ -0,0 +1,2 @@
+#include "SacrificeSelectorWidget.h"
+
---
+++ ldj43/Source/ldj43/UI/SacrificeSelectorWidget.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "SacrificeSelectorWidget.generated.h"
+
+UCLASS()
+class LDJ43_API USacrificeSelectorWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintImplementableEvent)
+	void Activate()
+	
+	
+};
