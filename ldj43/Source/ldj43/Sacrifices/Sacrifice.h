3cfdc1a98e490f32bdd5e25aa466e02e2a8ec248
c.fazilleau <c.fazilleau@student.isartdigital.com>
2018-12-02 19:59:11 +0100

    Worked on Sacrifices

--- ldj43/Source/ldj43/Sacrifices/Sacrifice.h
+++ ldj43/Source/ldj43/Sacrifices/Sacrifice.h
@@ -2,6 +2,7 @@
 
 #include "Sacrifices/Bonus.h"
 #include "Sacrifices/Malus.h"
+#include "Engine/Texture2D.h"
 
 #include "UObject/NoExportTypes.h"
 #include "Sacrifice.generated.h"
@@ -11,8 +12,21 @@ class LDJ43_API USacrifice : public UObject, public IBonus, public IMalus
 {
 	GENERATED_BODY()
 
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FString _bonusDescription = "unspecified";
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FString _malusDescription = "unspecified";
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UTexture2D* _image;
+
 public:
 	void Regenerate() override {}
+
+	UFUNCTION(BlueprintCallable)
 	void ApplyBonus(AMainCharacter* chara) override {}
+	UFUNCTION(BlueprintCallable)
 	void ApplyMalus(AMainCharacter* chara) override {}
 };
